f8d2cee51579c04ece10f3a4c6ffbbd7c893aabb
Jitendra Pal <55188975+threej-in@users.noreply.github.com>
2020-11-30 10:48:46 +0530

    Update functions.php
    added message_id parameter to pin_msg & unpin_msg method

--- src/includes/processors/functions.php
+++ src/includes/processors/functions.php
@@ -32,23 +32,28 @@ class jarvis_functions{
     /**
      * Executes curl_handler function from COM class in communicator.php
      */
-    private function execute($parameter, $r = 0, $is_req_chat_id = true)
+    private function execute($parameter, $is_chat_id_req = true)
     {
-        $is_req_chat_id == true && $parameter += CHATARR;
+        if(DEBUG_MODE === true){
+            $r = 1;
+        }else{
+            $r = 0;
+        }
+
+        $is_chat_id_req === true && $parameter += CHATARR;
         return COM::curl_handler($parameter, $r);
     }
 
     /**
      * Deletes existing message sent by bot in private chat and groups if have proper permission.
      * 
-     * @param bool $r -send 1 if response needed
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function delete_msg($r = 0){
+    function delete_msg(){
         
         return $this->execute([
             'method'=>'deletemessage',
-        ]+ MSGARR, $r);
+        ]+ MSGARR);
     }
     
     /**
@@ -59,15 +64,13 @@ class jarvis_functions{
      * ['inline_keyboard'=>[[[text & (url | login_url | callback_data |
      *  switch_inline_query | switch_inline_query_current_chat | callback_game | pay)]]]
      * ]
-     * @param bool $r -send 1 if response needed
      * @param string $inline_msg_id
      * @param bool $disable_web_preview
      * 
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
     function edit_msg(
         $text,
-        $r=0,
         $reply_markup=null,
         $inline_msg_id=null,
         $disable_web_preview=true)
@@ -79,7 +82,7 @@ class jarvis_functions{
             'reply_markup'=> $reply_markup,
             'inline_message_id'=>$inline_msg_id,
             'disable_webpage_preview'=> $disable_web_preview
-        ]+ MSGARR + PMHTML, $r);
+        ]+ MSGARR + PMHTML);
     }
 
     /**
@@ -90,15 +93,13 @@ class jarvis_functions{
      * ['inline_keyboard'=>[[[text & (url | login_url | callback_data |
      *  switch_inline_query | switch_inline_query_current_chat | callback_game | pay)]]]
      * ]
-     * @param bool $r -send 1 if response needed
      * @param string $inline_msg_id
      * 
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      * 
      */
     function edit_msg_caption(
         $caption,
-        $r=0,
         $reply_markup=null,
         $inline_msg_id=null)
     {
@@ -107,36 +108,33 @@ class jarvis_functions{
             'caption'=>substr($caption, 0,1022),
             'reply_markup'=> $reply_markup,
             'inline_message_id'=>$inline_msg_id,
-        ]+ MSGARR + PMHTML, $r);
+        ]+ MSGARR + PMHTML);
     }
 
     /**
      * forward message
      * @param int|string $from_chat
      * @param bool $send_notification
-     * @param bool $r -send 0 or 1 if response needed from curl_handler
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function forward_msg($from_chat, $r = 0, $send_notification = true){
+    function forward_msg($from_chat, $send_notification = true){
         
         return $this->execute([
             'method'=>'forwardmessage',
             'from_chat_id'=>$from_chat,
             'disable_notification'=>!($send_notification)
-        ]+ MSGARR, $r);
+        ]+ MSGARR);
     }
 
     /**
      * forward copied message
      * @param int|string $from_chat
      * @param bool $send_notification
-     * @param bool $r -send 0 or 1 if response needed from curl_handler
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
     function forward_copied_msg(
         $from_chat,
         $src_msg_id,
-        $r = 0,
         $caption = null,
         $reply_markup = "",
         $reply_to_original_msg = false,
@@ -151,50 +149,48 @@ class jarvis_functions{
             'allow_sending_without_reply'=>true,
             'disable_notification'=>!($send_notification),
             'reply_markup'=>$reply_markup
-        ] + PMHTML, $r);
+        ] + PMHTML);
     }
 
-    /**
+    /** 
      * bot will leave the chat
-     * @param bool $r -send 0 or 1 if response needed from curl_handler
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function leave_chat($r = 0){
+    function leave_chat(){
         return $this->execute([
             'method'=> 'leavechat',
-        ], $r);
+        ]);
     }
 
-    /**
+    /** 
      * Pin message
+     * @param int $pin_msg_id Message Id to be pinned
      * @param bool $send_notification
-     * @param bool $r -send 0 or 1 if response needed from curl_handler
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function pin_msg($send_notification = true, $r = 0){
+    function pin_msg($pin_msg_id ,$send_notification = true){
 
         return $this->execute([
             'method'=>'pinchatmessage',
+            'message_id'=>$pin_msg_id,
             'disable_notification'=>!($send_notification)
-        ]+ MSGARR, $r);
+        ]);
     }
 
-    /**
+    /** 
      * Removes the menu keyboard/button
      * @param string $msgtouser - Indicate user that you have removed the menu buttons.
-     * @param bool $r - pass 1 to get response
      * @param bool $selective - Buttons will be removed for selective user, useful in the group
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
     function remove_keyboard(
         $msgtouser,
-        $r = 0,
         $selective = true
         ){
         return $this->send_message($msgtouser,1,[
             'remove_keyboard'=>true,
             'selective'=> $selective
-        ], $r);
+        ]);
     }
 
     /**
@@ -202,16 +198,14 @@ class jarvis_functions{
      * @param mixed $msg_for_admin
      * @param int|double $chat_id
      * @param string $msg_for_user
-     * @param bool $r -send 0 or 1 if response needed from curl_handler
      * 
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
     function report_error(
         $msg_for_admin,
-        $msg_for_user = "Unknown error occured",
-        $r = 0
+        $msg_for_user = "Unknown error occured"
         ){
-        $this->send_message($msg_for_user,$r);
+        $this->send_message($msg_for_user);
         COM::send_log($msg_for_admin);        
     }
 
@@ -220,20 +214,19 @@ class jarvis_functions{
      *
      * @param string $action - Supported actions (typing | upload_photo | record_video | upload_video
      * | record_audio | upload_audio | upload_document | find_location | record_video_note | record_audio_note)
-     * @return NULL no return value
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function send_action($action = "typing", $r = 0){
+    function send_action($action = "typing"){
         
         return $this->execute([
             'method'=>'sendchataction',
             'action'=> $action
-        ],$r);
+        ]);
     }
     
-    /**
+    /** 
      * sends text message with reply_markup
      * @param string $text -text message to send
-     * @param bool $r send 1 if response needed from telegram
      * @param array $markup_parameter
      * Array of
      * *
@@ -262,11 +255,10 @@ class jarvis_functions{
      * @param bool $send_notification - send notification
      * @param bool $allow_wo_replymsg - Pass True, if the message should be sent even if the specified replied-to message is not found
      *
-     * @return array|bool - Curl response as array if $r set 1, else boolean
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
     function send_message(
         $text,
-        $r = 0,
         $markup_parameter = "",
         $reply_msgid = true,
         $noweb_preview = true,
@@ -282,29 +274,28 @@ class jarvis_functions{
             'disable_web_page_preview'=>$noweb_preview,
             'disable_notification'=>!$send_notification,
             'allow_sending_without_reply'=> $allow_wo_replymsg
-        ] + PMHTML, $r);
+        ] + PMHTML);
     }
     
-    /**
+    /** 
      * Send sticker if allowed
      * @param string $file_id
-     * @param bool $r send 1 if response needed from telegram api
-     * @return bool|array 
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function send_sticker($file_id, $r = 0){
+    function send_sticker($file_id){
 
         return $this->execute([
             'method' => 'sendsticker',
             'sticker' => $file_id
-        ],$r);
+        ]);
     }
 
-    /**
+    /** 
      * Set bot commands
      * @param array $param Array consisting list of bot command as key and its description as value
-     * @return array
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
      */
-    function set_commands($parameter, $r){
+    function set_commands($parameter){
         $botcommand = [];
         $i = 0;
         foreach($parameter as $k => $v){
@@ -317,21 +308,30 @@ class jarvis_functions{
         return $this->execute([
             'method'=>'setmycommands',
             'commands'=>$botcommand
-        ], $r, false);
+        ], false);
     }
 
-    function unpin_msg($r = 0){
+    /**
+     * Unpin message function
+     * @param int $unpin_msg_id
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
+     */
+    function unpin_msg($unpin_msg_id){
 
         return $this->execute([
             'method'=>'unpinchatmessage',
-        ], $r);
+            'message_id'=>$unpin_msg_id
+        ]);
     }
 
-    
-    function unpin_all_msg($r = 0){
+    /**
+     * Unpin all messages, It is recommended to confirm once with user before executing this method
+     * @return array|bool - Curl response as array if DEBUGMODE is set true, else bool
+     */
+    function unpin_all_msg(){
 
         return $this->execute([
             'method'=>'unpinallchatmessages',
-        ],$r);
+        ]);
     }
 }
